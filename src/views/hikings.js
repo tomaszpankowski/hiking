d661ae3884750dc4a2f7de1b2f25c1d99dfdafbd
tomaszpankowski <pankowski.email@gmail.com>
2022-02-11 01:32:15 +0100

    layout done

--- src/views/hikings.js
+++ src/views/hikings.js
@@ -1,11 +1,16 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import HikingsS1 from "../components/hikings-s1";
+import HikingsS2 from "../components/hikings-s2";
+import HikingsS3 from "../components/hikings-s3";
 
 class Hikings extends Component{
     render(){
         return(        
-            <Container fluid className="hikings-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <HikingsS1/>
+                <HikingsS2/>
+                <HikingsS3/>
             </Container>    
         );
     }
--- src/views/tips.js
+++ src/views/tips.js
@@ -1,11 +1,18 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import TipsS1 from "../components/tips-s1";
+import TipsS2 from "../components/tips-s2";
+import TipsS3 from "../components/tips-s3";
+import TipsS4 from "../components/tips-s4";
 
 class Tips extends Component{
     render(){
         return(        
-            <Container fluid className="tips-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <TipsS1/>
+                <TipsS2/>
+                <TipsS3/>
+                <TipsS4/>
             </Container>    
         );
     }
